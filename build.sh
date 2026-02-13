fdc404a9780bf243435e302c83fc5e085f97ec6f
khadidja mechara <mecharakhadidja71@gmail.com>
2026-02-13 05:24:49 +0100

    mod

--- build.sh
+++ build.sh
@@ -10,3 +10,13 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123456')
+    print("✅ Superuser created successfully!")
+else:
+    print("✅ Superuser already exists.")
+EOF
