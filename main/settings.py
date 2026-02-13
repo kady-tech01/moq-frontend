f21d74ceb80d9918519f11a456ad9a7f9e2942a6
khadidja mechara <mecharakhadidja71@gmail.com>
2026-02-13 01:19:17 +0100

    mod

--- main/settings.py
+++ main/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-CORS_ALLOW_ALL_ORIGINS = 
+CORS_ALLOW_ALL_ORIGINS = True    
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3000',
