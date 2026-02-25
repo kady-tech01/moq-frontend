8a0e9c305d9d28ff4a940941e68ae226d6a7db5a
khadidja mechara <mecharakhadidja71@gmail.com>
2026-02-25 17:56:52 +0100

    ff

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css"; // optional
+import "./index.css"; 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+// 1. استيراد تابع الحقن الخاص بـ Vercel Analytics
+import { inject } from '@vercel/analytics';
 
+// 2. تفعيل التتبع (يفضل وضعه قبل الـ Render)
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
